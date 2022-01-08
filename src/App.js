44487b109c56d8dcb8ffdd41e95c35ef98932142
harshjha01 <harsh1998mrperfect@gmail.com>
2022-01-08 11:25:28 +0530

    .
    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calc from "./Componments/Calc";
+
+function App() {
+  return (
+    <div className="App">
+      <Calc></Calc>
+    </div>
+  );
+}
+
+export default App;
